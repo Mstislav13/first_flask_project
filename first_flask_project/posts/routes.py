be8330e14dcf6db50d5d3037121047753a14a40b
mstislav <strong13strong@gmail.com>
2022-07-22 17:41:52 +0700

    lesson_7

--- first_flask_project/posts/routes.py
+++ first_flask_project/posts/routes.py
@@ -2,8 +2,9 @@ from flask import (render_template, url_for, flash, redirect, request, abort,
                    Blueprint)
 from flask_login import current_user, login_required
 from first_flask_project import db
-from first_flask_project.models import Post, Comments
-from first_flask_project.posts.forms import PostForm
+from first_flask_project.models import Post, Comment
+from first_flask_project.posts.forms import PostForm, CommentForm
+from first_flask_project.posts.utils import save_picture
 
 posts = Blueprint('posts', __name__)
 
@@ -29,15 +30,20 @@ def new_post():
     :return:
     """
     form = PostForm()
+    if request.method == 'POST':
+        if form.picture.data:
+            picture_name = save_picture(form.picture.data)
     if form.validate_on_submit():
         post = Post(title=form.title.data, description=form.description.data,
-                    content=form.content.data, author=current_user)
+                    content=form.content.data, author=current_user,
+                    image_file=picture_name)
         db.session.add(post)
         db.session.commit()
         flash('Ваш пост создан!', 'success')
         return redirect(url_for('posts.allpost'))
     return render_template('create_post.html',
-                           title='Новый пост', form=form, legend='Новый пост')
+                           title='Новый пост', image_file=form.picture.data,
+                           form=form, legend='Новый пост')
 
 
 @posts.route("/post/<int:post_id>", methods=['GET', 'POST'])
@@ -48,21 +54,16 @@ def post(post_id):
     :return:
     """
     post = Post.query.get_or_404(post_id)
-    comment = Comments.query.filter_by(post_id=post.id).all()
-    if request.method == 'POST':
-        post_id = post.id
-        name = request.form.get('name')
-        email = request.form.get('email')
-        content = request.form.get('content')
-        comment = Comments(name=name, email=email, content=content,
-                           post_id=post_id)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(content=form.comment.data, post_id=post_id,
+                          username=current_user.username)
         db.session.add(comment)
         db.session.commit()
         flash('Ваш комментарий был создан.', 'success')
         return redirect(request.url)
 
-    return render_template('post.html', title=post.title, post=post,
-                           comment=comment)
+    return render_template('post.html', title=post.title, post=post, form=form)
 
 
 @posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
@@ -81,6 +82,9 @@ def update_post(post_id):
         post.title = form.title.data
         post.description = form.description.data
         post.content = form.content.data
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            post.image_file = picture_file
         db.session.commit()
         flash('Ваш пост обновлен!', 'success')
         return redirect(url_for('posts.post', post_id=post.id))
@@ -88,7 +92,10 @@ def update_post(post_id):
         form.title.data = post.title
         form.description.data = post.description
         form.content.data = post.content
+        image_file = url_for('static', filename='posts_img/' +
+                                                post.image_file)
     return render_template('update_post.html', form=form,
+                           image_file=image_file,
                            legend='Редактирование поста')
 
 
@@ -106,4 +113,21 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     flash('Ваш пост был удален!', 'success')
+    return redirect(url_for('posts.allpost'))
+
+
+@posts.route("/comment/<int:comment_id>/delete", methods=['POST'])
+@login_required
+def delete_comment(comment_id):
+    """
+    Удаление комментария
+    :param comment_id:
+    :return:
+    """
+    comment = Comment.query.get_or_404(comment_id)
+    if comment.username != current_user.username:
+        abort(403)
+    db.session.delete(comment)
+    db.session.commit()
+    flash('Ваш комментарий был удален!', 'success')
     return redirect(url_for('posts.allpost'))
---
+++ first_flask_project/posts/utils.py
@@ -0,0 +1,23 @@
+import os
+from secrets import token_hex
+from PIL import Image
+from flask import url_for, current_app
+
+
+def save_picture(form_picture):
+    """
+    Сохранение картинок к посту
+    :param form_picture:
+    :return:
+    """
+    random_hex = token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/posts_img',
+                                picture_fn)
+    output_size = (300, 300)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
